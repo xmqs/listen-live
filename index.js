b340a9c6a09351e40c95633302570b819ceae685
wuyongxiang <wuyongxiang@zhuooo.com>
2025-08-15 10:00:19 +0800

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process');
+const readline = require('readline');
+const path = require('path');
+
+const ffplayPath = path.join(__dirname, './ffplay/ffplay.exe');
+const baseUrl = 'https://live.iill.top/';
+
+// Platform 选项
+const platforms = ['douyu', 'huya'];
+let platformIndex = 0;
+
+// 创建 readline 接口
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+// 第一步：选择 Platform
+function renderPlatformMenu() {
+    console.clear();
+    console.log('使用 ↑ ↓ 选择平台，按 Enter 确认\n');
+
+    platforms.forEach((p, i) => {
+        if (i === platformIndex) {
+            console.log(`> ${p}`);
+        } else {
+            console.log(`  ${p}`);
+        }
+    });
+}
+
+renderPlatformMenu();
+
+process.stdin.on('keypress', (str, key) => {
+    console.clear()
+    if (key.name === 'up') {
+        platformIndex = (platformIndex - 1 + platforms.length) % platforms.length;
+    } else if (key.name === 'down') {
+        platformIndex = (platformIndex + 1) % platforms.length;
+    } else if (key.name === 'return') {
+        // 用户确认 Platform 选择，进入输入阶段
+        process.stdin.setRawMode(false);
+        rl.question('room: ', (input) => {
+            const finalUrl = `${baseUrl}/${platforms[platformIndex]}/${encodeURIComponent(input)}`;
+
+            const ffplay = spawn(ffplayPath, [
+                finalUrl,
+                '-vn',
+                '-nodisp'
+            ], { stdio: 'ignore' });
+
+            ffplay.on('close', (code) => {
+                console.log(`直播流可能存在问题， ${code}`);
+                rl.close();
+                process.exit();
+            });
+        });
+        return;
+    } else if (key.ctrl && key.name === 'c') {
+        rl.close();
+        process.exit();
+    }
+});
---
+++ readme.md
@@ -0,0 +1,34 @@
+# listen-live
+### 主要功能：听
+Terminalm 控制台启动 douyu/huya 直播
+
+集成ffplay播放器，播放直播流的音频
+# 开发原因
+### 听
+主要自己想上班听枫哥直播（rua）
+
+大部分程序员上班能听，但是不太方便看直播（狗头）
+
+满足听直播需求
+
+# 直播流来源
+https://github.com/YanG-1989/m3u
+# 食用方法
+
+npm i listen-live
+
+listen-live
+
+选择平台 比如：douyu
+
+输入room 房间号：9999
+
+（rua）
+
+# 后续开发
+
+#### 更多直播源
+
+#### 订阅模式
+
+#### 在线更新
